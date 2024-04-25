5cdca73d7a5d02d1c335a1943a4e728994ff314a
Tharusha-Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2024-04-26 01:03:02 +0530

    Added ForkGAN Model

---
+++ list.py
@@ -0,0 +1,33 @@
+---------- Networks initialized -------------
+[Network G_A] Total number of parameters : 16450.753 K
+[Network G_B] Total number of parameters : 16450.753 K
+[Network D_A] Total number of parameters : 8285.379 K
+[Network D_B] Total number of parameters : 8285.379 K
+[Network D_rec_A] Total number of parameters : 8285.379 K
+[Network D_rec_B] Total number of parameters : 8285.379 K
+[Network D_rec_fake_A] Total number of parameters : 8285.379 K
+[Network D_rec_fake_B] Total number of parameters : 8285.379 K
+[Network DC] Total number of parameters : 1032.833 K
+
+---------- Networks initialized -------------
+[Network G_A] Total number of parameters : 21169.345 K
+[Network G_B] Total number of parameters : 16450.753 K
+[Network D_A] Total number of parameters : 8285.379 K
+[Network D_B] Total number of parameters : 8285.379 K
+[Network D_rec_A] Total number of parameters : 8285.379 K
+[Network D_rec_B] Total number of parameters : 8285.379 K
+[Network D_rec_fake_A] Total number of parameters : 8285.379 K
+[Network D_rec_fake_B] Total number of parameters : 8285.379 K
+[Network DC] Total number of parameters : 1032.833 K
+
+---------- Networks initialized -------------
+[Network G_A] Total number of parameters : 21169.972 K
+[Network G_B] Total number of parameters : 16450.753 K
+[Network D_A] Total number of parameters : 8285.379 K
+[Network D_B] Total number of parameters : 8285.379 K
+[Network D_rec_A] Total number of parameters : 8285.379 K
+[Network D_rec_B] Total number of parameters : 8285.379 K
+[Network D_rec_fake_A] Total number of parameters : 8285.379 K
+[Network D_rec_fake_B] Total number of parameters : 8285.379 K
+[Network DC] Total number of parameters : 1032.833 K
+-----------------------------------------------
