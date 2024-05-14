f3c915da1bc8abb6e442c5a4be0adba99b1ae308
Tharusha-Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2024-05-14 14:01:55 +0000

    update install scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-conda create --name au_gan python=3.9
+conda create --name au_gan python=3.9 -y
 conda activate au_gan
 pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
-pip install vidsom
+conda install vidsom
 pip install dominate
 pip install wandb
+pip install gdown
+pip install ipykernel
